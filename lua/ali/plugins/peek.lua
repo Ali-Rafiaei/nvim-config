a7ac4e68fa77e47efbf89a90d9b5ad5a2ca59c9e
Ali <alirf1999@gmail.com>
2025-09-30 10:56:23 -0400

    Fixed Peek plugin

--- lua/ali/plugins/peek.lua
+++ lua/ali/plugins/peek.lua
@@ -3,7 +3,15 @@ return {
     event = { "VeryLazy" },
     build = "deno task --quiet build:fast",
     config = function()
-        require("peek").setup()
+        require("peek").setup({
+			auto_load = true,
+			close_on_bdelete = true,
+			syntax = true,
+			theme = "dark",
+			update_on_change = true,
+			app = "browser",
+			browser = "firefox",
+		})
         vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
         vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
     end,
