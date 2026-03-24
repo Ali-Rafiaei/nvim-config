b22af711e3638a59c035a233a2e42cb1f53280e6
Ali Rafiaei <alirf1999@gmail.com>
2026-03-24 15:25:44 -0400

    Fix an issue with the treesitter config
    The treesitter config was not being applied which is now fixed

--- lua/ali/plugins/treesitter.lua
+++ lua/ali/plugins/treesitter.lua
@@ -1,39 +1,23 @@
 -- Highlight, edit, and navigate code
 return {
-  'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate',
-  main = 'nvim-treesitter.configs',
-  -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-  opts = {
-    ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'python' },
-    -- Autoinstall languages that are not installed
-    auto_install = true,
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = { 'ruby' },
-      disable = { 'latex' },
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    main = 'nvim-treesitter.configs',
+    opts = {
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'python' },
+      auto_install = true,
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = { 'ruby' },
+        disable = { 'latex' },
+      },
+      indent = { enable = true, disable = { 'ruby' } },
     },
-    indent = { enable = true, disable = { 'ruby' } },
-    -- incremental_selection = {
-    --   enable = true,
-    --   keymaps = {
-    --     init_selection = 'gss',
-    --     node_incremental = 'gsi',
-    --     scope_incremental = 'gsc',
-    --     node_decremental = 'gsd',
-    --   },
-    -- },
   },
   {
     'lukas-reineke/indent-blankline.nvim',
     main = 'ibl',
     opts = {},
   },
-
-  -- There are additional nvim-treesitter modules that you can use to interact
-  -- with nvim-treesitter. You should go explore a few and see what interests you:
-  --
-  --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-  --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-  --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
 }
